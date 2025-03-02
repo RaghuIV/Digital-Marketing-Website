c15010f15bf070f3bd12cf74f149f5d1b92f0edf
raghu <raghunathsingh2410@gmail.com>
2025-03-02 18:12:18 +0530

    mobile optimised

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ setInterval(() => {
         mainTexts[0].classList.add('change');
     }
 }, 3000);
+
+const closeNav = document.querySelector('.close-menu i')
+const navMobile = document.querySelector('.nav-bar-mb-menu')
+const dateValue = new Date;
+const yearValue = dateValue.getFullYear();
+const copyRightText = document.querySelector('.copyright')
+copyRightText.innerHTML = `<p>Copyright <i class="fa-solid fa-copyright"></i> ${yearValue}, All Rights Reserved</p>`
+
+const hamburgerIcon = document.querySelector('.hamburger-icon')
+
+
+hamburgerIcon.addEventListener('click',()=>{
+  navMobile.classList.remove('no')
+})
+
+document.addEventListener('scroll',()=>{
+    navMobile.classList.add('no')
+})
+
+closeNav.addEventListener('click',()=>{
+    navMobile.classList.add('no')
+})
